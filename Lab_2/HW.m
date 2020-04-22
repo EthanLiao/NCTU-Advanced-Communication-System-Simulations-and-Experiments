adb6f0a224e028964af8e675895cdd4d444cdbfb
Ethan Liao <simon7766112233.eed04@nctu.edu.tw>
2020-04-22 22:28:56 +0800

    HW2 modify

--- Lab_2/HW.m
+++ Lab_2/HW.m
@@ -1,11 +1,25 @@
+clf; clear all;
+% HW 1
+fs_orig = 4 * 10^6
+[sin_sig,sin_time,fft_sin] = sinusoid_with_fft(fs_orig)
+fs_another = 1.5 * 10^6
+[sin_sig,sin_time,fft_sin] = sinusoid_with_fft(fs_another)
+
+figure()
+subplot(4,1,1);stem(sin_time,sin_sig,'.-');title('Sinusoid Signal');xlabel('Time(seconds)'); ylabel('Amplitude'); grid on;
+subplot(4,1,2);stem(abs(fft_sin),'.-b') ; xlabel('Frequency'); ylabel('Magnitude'); grid on;
+subplot(4,1,3);stem(sin_time,sin_sig,'.-');title('Sinusoid Signal');xlabel('Time(seconds)'); ylabel('Amplitude'); grid on;
+subplot(4,1,4);stem(abs(fft_sin),'.-b') ; xlabel('Frequency'); ylabel('Magnitude'); grid on;
+
+
+
+% HW 2
 ele_nums = 200
 mid = ceil(ele_nums/2)
 half = 30
-
 % rectangular pulse
 rect = zeros(1, ele_nums)
 rect(mid-half : mid+half) = 1
-
 
 % Convolve
 triangular = conv(rect, rect)
@@ -17,6 +31,8 @@ Second_moment_noise = moment(triangular,2) / (10^(SNR/10))
 noise =  Second_moment_noise*randn(1,convolve_Length) / 3
 tri_with_noise = noise + triangular
 fft_y =  fft(tri_with_noise)
+
+figure()
 subplot(5, 1, 3); plot(abs(fft_y)); grid 'on';
 
 % filtering white noise in the frequency domain
@@ -58,9 +74,14 @@ subplot(5, 1, 1); plot(triangular); grid 'on';
 subplot(5, 1, 2); plot(tri_with_noise); grid 'on'; title(['SNR: ',num2str(orig_SNR),' dB']);
 subplot(5, 1, 4); plot(abs(fft_y)); grid 'on';
 subplot(5, 1, 5); plot(ifft(fft_y)); grid 'on'; title(['SNR: ',num2str(new_SNR),' dB;','MSE: ',num2str(mean_square_error)]);
-% subplot(5, 1, 4); plot(real_window); grid 'on'; title(['size ',num2str(mean_square_error),' window']);
 
 
-% function err = cal_mean_square_error(signal,orig_signal)
-%   err = sum((signal-orig_signal).^2)
-% end
+function [sig,sig_time,fft_sig] = sinusoid_with_fft(fs)
+  N = 3
+  % freqStep = fs / N
+  fc = 10^6 % fc = 1 * freqStep
+  sig_time = (0:0.01:N) / fc
+  sig = cos(2*pi*fc*sig_time)
+  fft_sig = fft(sig)
+  fft_sig = fftshift(fft_sig)
+end
