1dfc4dbc6f51c6704c538b6210815f03e4629fff
Ethan Liao <simon7766112233.eed04@nctu.edu.tw>
2020-05-11 18:23:11 +0800

    Lab9 final version

--- Lab_9/HW.m
+++ Lab_9/HW.m
@@ -1,37 +1,64 @@
 clf;close all;clear all
-
-sig = [-1,1,-1,-1,1,-1,-1,-1,-1]
+N = 20
+sig = randi([0,1],1,N)
+sig((sig==0)) = -1
 
 symbol_rate = 1*10^6
-carrier_frequency = 16*10^6
+carrier_frequency = 8*10^6
 IF_frequency = 4*10^6
+freq_DAC = 16*10^6
+freq_DMA = 64*10^6
+f_DAC = 16
+f_DMA = 4
+SNR_DB = 0.01
 
-srrc_2 = srrc_pulse(2, 10, 0.3);
-srrc_16 = srrc_pulse(16,10, 0.3);
+srrc_16 = srrc_pulse(16, 5, 0.3);
+srrc_4 = srrc_pulse(4,5,0.3)
+
+srrc_16_length = length(srrc_16)
+srrc_4_length = length(srrc_4)
 
 load('IIR_filter')
-
-t_DAC_sig = conv(DAC(sig,16),srrc_2,'same')
-t_DMA_sig = filter(IIR_filter,DAC(t_DAC_sig,4))
-
-
+load('clr_filter')
 % modulatoin part
-t_vec = [1:length(t_DMA_sig)]
-carrier = cos(2*pi*(carrier_frequency-IF_frequency)/(32*10^6)*t_vec)
-sig_with_carrier = t_DMA_sig .* carrier
+t_DAC_sig = conv(DAC(sig,f_DAC),srrc_16)
+t_DAC_sig = t_DAC_sig((srrc_16_length-1)/2+1:end-(srrc_16_length-1)/2)
 
-t_DMA_sig = ADC(conv(sig_with_carrier,srrc_2,'same'),4)
+t_DMA_ana_sig = conv(DAC(t_DAC_sig,f_DMA),srrc_4)
+t_DMA_ana_sig = t_DMA_ana_sig((srrc_4_length-1)/2+1:end-(srrc_4_length-1)/2)
+
+t_DMA_sig = filter(IIR_filter,t_DMA_ana_sig)
+
+% modulatoin with carrier
+t = [0:length(t_DMA_sig)-1]
+carrier = sqrt(2)*exp(1j*2*pi*carrier_frequency*t)
+sig_with_carrier = real(t_DMA_sig .* carrier)
+
+awgn_sig = add_awgn_noise(sig_with_carrier,SNR_DB)
+
+% Image rejection Filter
+
+clear_sig = filter(clr_filter,awgn_sig)
+
+% IF Band
+t_vec = [1:length(clear_sig)]
+carrier = cos(2*pi*(carrier_frequency-IF_frequency)/freq_DMA*t_vec)
+IF_sig = clear_sig .* carrier
+r_DMA_f_sig = conv(IF_sig,srrc_4)
+r_DMA_f_sig = r_DMA_f_sig((srrc_4_length-1)/2+1:end-(srrc_4_length-1)/2)
+r_DMA_sig = ADC(r_DMA_f_sig,f_DMA)
 
 % demodulatoin with carrier
-t = [0:length(t_DMA_sig)-1]
-carrier = exp(i*(2*pi*IF_frequency/(16*10^6)*t))
-demod_sig = t_DMA_sig .* carrier
+t = [0:length(r_DMA_sig)-1]
+carrier = exp(-1j*2*pi*IF_frequency/freq_DAC*t)
+demod_sig = real(r_DMA_sig .* carrier)
 
 % demodulation
-r_ADC_sig = ADC(conv(demod_sig,srrc_16,'same'),16)
+r_ADC_f_sig = conv(demod_sig,srrc_16)
+r_ADC_f_sig = r_ADC_f_sig((srrc_16_length-1)/2+1:end-(srrc_16_length-1)/2)
+r_ADC_sig = ADC(r_ADC_f_sig,f_DAC)
 
 r_sig = r_ADC_sig./max(abs(r_ADC_sig))
-
 rcv_sig = (r_sig>0) + (-(r_sig<0))
 
 subplot(2,1,1);stem(sig);title('BPSK Signal');grid on;
@@ -56,21 +83,20 @@ function [y,t] = srrc_pulse(T,A,a)
     num = cos((1+a)*pi*t/T) + T*sin((1-a)*pi*t/T)./(4*a*t)
     denom = 1-(4*a*t/T).^2
     y = 4*a/pi * num./denom
-    y = y/max(y)
   else
-    y = 1/T * sin(pi.*t./T) / (pi.*t./T)
-    y = y/max(y)
+    y = 1/T * sin(pi.*t./T) ./ (pi*t./T)
   end
 end
 
-function y = add_awgn(x,SNR_DB)
-  len = length(x)
+function y = add_awgn_noise(x,SNR_DB)
   snr = 10^(SNR_DB/10)
-  SYME = abs(x).^2 / len
+  L = length(x)
+  SYME = sum(abs(x).^2) / L
   N0 = SYME / snr
   if isreal(x)
-    n = sqrt(N0) * randn(1,len)
+    n = sqrt(N0)*randn(1,L)
   else
-    n = sqrt(N0/2) * (randn(1,len)+j*randn(1,len))
-  y = x+n
+    n = sqrt(N0/2)*(randn(1,L)+j*randn(1,L))
   end
+  y = x+n
+end
--- Lab_9/practice3.m
+++ Lab_9/practice3.m
@@ -1,49 +1,73 @@
 clf;close all;clear all
-
-signal = [-1,1,-1,-1,1,-1,-1,-1,-1]
+N = 20
+sig = randi([0,1],1,N)
+sig((sig==0)) = -1
 
 symbol_rate = 1*10^6
 carrier_frequency = 8*10^6
 IF_frequency = 4*10^6
-srrc_2 = srrc_pulse(2, 10, 0.3);
-srrc_16 = srrc_pulse(16,10, 0.3);
+freq_DAC = 16*10^6
+freq_DMA = 32*10^6
+f_DAC = 16
+f_DMA = 2
+
+srrc_16 = srrc_pulse(16, 5, 0.3);
+srrc_2 = srrc_pulse(2,5,0.3)
+
+srrc_16_length = length(srrc_16)
+srrc_2_length = length(srrc_2)
 
 load('IIR_filter')
-t_Digital_filtered_signal = conv(DAC(signal,16),srrc_2,'same')
-t_DMA_filtered_signal = filter(IIR_filter,DAC(t_Digital_filtered_signal,2))
-
 
 % modulatoin part
-t_vec = [1:length(t_DMA_filtered_signal)]
-carrier = cos(2*pi*(carrier_frequency-IF_frequency)/(32*10^6)*t_vec)
+t_DAC_sig = conv(DAC(sig,f_DAC),srrc_16)
+t_DAC_sig = t_DAC_sig((srrc_16_length-1)/2+1:end-(srrc_16_length-1)/2)
 
-signal_with_carrier = t_DMA_filtered_signal .* carrier
-t_DMA_filtered_signal = ADC(conv(signal_with_carrier,srrc_2,'same'),2)
+t_DMA_ana_sig = conv(DAC(t_DAC_sig,f_DMA),srrc_2)
+t_DMA_ana_sig = t_DMA_ana_sig((srrc_2_length-1)/2+1:end-(srrc_2_length-1)/2)
 
-% de modulatoin with carrier
-t = [0:length(t_DMA_filtered_signal)-1]
-carrier = exp(i*(2*pi*IF_frequency/(16*10^6)*t))
-demodulation_signal = t_DMA_filtered_signal .* carrier
+t_DMA_sig = filter(IIR_filter,t_DMA_ana_sig)
+
+% modulatoin with carrier
+t = [0:length(t_DMA_sig)-1]
+carrier = sqrt(2)*exp(1j*2*pi*carrier_frequency*t)
+sig_with_carrier = real(t_DMA_sig .* carrier)
+
+% IF Band
+t_vec = [1:length(sig_with_carrier)]
+carrier = cos(2*pi*(carrier_frequency-IF_frequency)/freq_DMA*t_vec)
+
+IF_sig = sig_with_carrier .* carrier
+r_DMA_f_sig = conv(IF_sig,srrc_2)
+r_DMA_f_sig = r_DMA_f_sig((srrc_2_length-1)/2+1:end-(srrc_2_length-1)/2)
+r_DMA_sig = ADC(r_DMA_f_sig,f_DMA)
+
+% demodulatoin with carrier
+t = [0:length(r_DMA_sig)-1]
+carrier = exp(-1j*2*pi*IF_frequency/freq_DAC*t)
+demod_sig = real(r_DMA_sig .* carrier)
 
 % demodulation
-r_Digital_filtered_signal = ADC(conv(demodulation_signal,srrc_16,'same'),16)
+r_ADC_f_sig = conv(demod_sig,srrc_16)
+r_ADC_f_sig = r_ADC_f_sig((srrc_16_length-1)/2+1:end-(srrc_16_length-1)/2)
+r_ADC_sig = ADC(r_ADC_f_sig,f_DAC)
 
-r_sig = r_Digital_filtered_signal./max(abs(r_Digital_filtered_signal))
+r_sig = r_ADC_sig./max(abs(r_ADC_sig))
 rcv_sig = (r_sig>0) + (-(r_sig<0))
 
-subplot(2,1,1);stem(signal);title('BPSK Signal');grid on;
+subplot(2,1,1);stem(sig);title('BPSK Signal');grid on;
 subplot(2,1,2);stem(rcv_sig);title('Recieved Signal');grid on;
 figure()
-plot(abs(fft(demodulation_signal)))
+plot(abs(fft(demod_sig)))
 
-function DAC_sig = DAC(origin_signal,up_factor)
-  DAC_sig = zeros(1,up_factor*length(origin_signal))
-  DAC_sig([1:up_factor:length(DAC_sig)]) = origin_signal
+function DAC_sig = DAC(origin_sig,up_factor)
+  DAC_sig = zeros(1,up_factor*length(origin_sig))
+  DAC_sig([1:up_factor:length(DAC_sig)]) = origin_sig
 end
 
-function ADC_sig = ADC(origin_signal,down_factor)
-  ADC_sig = zeros(1,length(origin_signal))
-  ADC_sig = origin_signal(1:down_factor:end)
+function ADC_sig = ADC(origin_sig,down_factor)
+  ADC_sig = zeros(1,length(origin_sig))
+  ADC_sig = origin_sig(1:down_factor:end)
 end
 
 % SRRC Pulse
@@ -54,6 +78,6 @@ function [y,t] = srrc_pulse(T,A,a)
     denom = 1-(4*a*t/T).^2
     y = 4*a/pi * num./denom
   else
-    y = 1/T * sin(pi.*t./T) / (pi.*t./T)
+    y = 1/T * sin(pi.*t./T) ./ (pi*t./T)
   end
 end
