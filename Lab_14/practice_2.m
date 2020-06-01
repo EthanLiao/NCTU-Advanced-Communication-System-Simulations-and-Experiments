5d605668d3cfdd13cad4fedf5d625ed5b1517ab9
Ethan Liao <simon7766112233.eed04@nctu.edu.tw>
2020-06-01 11:57:27 +0800

    Lab 14 modify

--- Lab_14/practice_2.m
+++ Lab_14/practice_2.m
@@ -2,15 +2,37 @@ clf;clear all;close all;
 h = [0.1, 0.2, 0.3, 0.4];
 h_delay = (length(h)-1)/2;
 
-N = 20;
+N = 2;
 variance = 1;
 noise_sig = sqrt(variance)*randn(1, N);
-var_sig = mean((noise_sig-mean(noise_sig)).^2)
-
-filt_sig = conv(noise_sig,h);
-filt_sig = filt_sig(h_delay+1:end-h_delay);
-
+var_sig = mean((noise_sig-mean(noise_sig)).^2);
+filt_sig = float_operation(noise_sig,h);
 histogram(filt_sig);title("Dinamic range of input");
+
+
+function y = float_operation(x,h)
+  tap = zeros(1:length(x)-1);
+  z_n = x;
+  for i = 1:4
+    if i == 1
+      y = h(i) * x;
+    else
+      z_n = [tap z_n];
+      y = [y tap] + h(i) * z_n;
+    end
+  end
+
+  % tap = zeros(1:length(x)-1)
+  % z_1 = [tap x];
+  % y = h(1) * [x tap] + h(2) * z_1;
+  %
+  % z_2 = [tap, z_1];
+  % y =  [y tap] + h(3) * z_2;
+  %
+  % z_3 = [tap z_2];
+  % y = [y tap] + h(4) * z_3;
+
+end
 
 function y = add_awgn_noise(x,SNR_DB)
   L = length(x);
